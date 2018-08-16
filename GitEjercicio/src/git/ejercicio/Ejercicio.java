5b733178961025ccde20db89d14bc921a95301a9
Federico Arambarri <federico.arambarri@honeywell.com>
2018-08-15 22:24:19 -0300

    una implementacion

--- GitEjercicio/src/git/ejercicio/Ejercicio.java
+++ GitEjercicio/src/git/ejercicio/Ejercicio.java
@@ -5,7 +5,7 @@ public class Ejercicio {
 	public static void main(String[] args) {
 		System.out.println("Primos: ");
 		for (int i = 0; i < 100; i++) {
-			System.out.println("");
+			System.out.println(i%2==0?i:" ");
 		}
 	}
 
